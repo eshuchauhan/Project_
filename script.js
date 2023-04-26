69e0a0a2104dfd2bff9d8313603c0d55f7937ef3
Deepak <deepakanjanigupta432@gmail.com>
2023-04-27 00:20:13 +0530

    Addition of Table and Saving Data in Local Storage

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function containsNumericOrSpecial(str) {
     for (var i = 0; i < str.length; i++) {
       var charCode = str.charCodeAt(i);
       if ((charCode < 48 || charCode > 57) && (charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122)) {
-        return false;
+          return false;
       }
     }
     return true;
@@ -22,5 +22,72 @@ function isValidMobileNumber(mobileNumber) {
   return mobileNumberRegex.test(mobileNumber);
 }
 
+var i =null;
+
+const button = document.querySelector('#submit');
+
+button.addEventListener('click', function() {
+  console.log('Button clicked!');
+  
+  var st_name = document.getElementById('name').value;
+
+  var st_email = document.getElementById('email').value;
+
+  var st_phone = document.getElementById('phone').value;
+
+  var flag = true;
+
+  if(containsNumericOrSpecial(st_name) === false){
+    flag =false;
+    window.alert("Please Enter Valid Name");
+  }
+
+  else if(isValidEmail(st_email) === false){
+    flag = false;
+    window.alert("Please Enter Valid Email");
+  }
+
+  if(isValidMobileNumber(st_phone)=== false){
+    flag = false;
+    window.alert("Please Enter Valid Mobile Number");
+  }
+  
+  
 
 
+  if(flag === true){
+  // const obj = {"name": st_name , "email" : st_email , "phone" :st_phone};
+  // localStorage.setItem("dummydata", JSON.stringify(obj));
+  if(localStorage.getItem('itemJson') == null){
+    itemJsonArray =[];
+    itemJsonArray.push([ st_name, st_phone , st_email]);
+    localStorage.setItem('itemJson', JSON.stringify(itemJsonArray))
+  }
+  else{
+    itemJsonArrayStr = localStorage.getItem('itemJson');
+    itemJsonArray = JSON.parse(itemJsonArrayStr);
+    itemJsonArray.push([  st_name, st_phone , st_email]);
+    localStorage.setItem('itemJson', JSON.stringify(itemJsonArray));
+   
+
+    // submit.preventDefault(); 
+
+    // document.getElementById("result").innerHTML = localStorage.getItem("itemJson");
+
+    if(i==null){
+      i= 0;
+    }
+    for (  i = i; i < itemJsonArray.length; ++i) {
+      var rowCount = myTable.rows.length;
+      var row = myTable.insertRow(rowCount);
+      row.insertCell(0).innerHTML= i+1;
+      row.insertCell(1).innerHTML= itemJsonArray[i][0];
+      row.insertCell(2).innerHTML= itemJsonArray[i][1];
+      row.insertCell(3).innerHTML= itemJsonArray[i][2];
+      i=i;
+    }
+  }
+
+
+}
+});
